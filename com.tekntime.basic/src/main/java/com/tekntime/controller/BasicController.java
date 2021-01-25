6708439ebb08d44cc50ce7755c30a0ad9f79cec5
susan <susan@172.29.0.148>
2021-01-25 10:56:41 -0500

    changed context path

--- com.tekntime.basic/src/main/java/com/tekntime/controller/BasicController.java
+++ com.tekntime.basic/src/main/java/com/tekntime/controller/BasicController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/basic")
+@RequestMapping("/auth")
 public class BasicController {
 	Logger logger = LoggerFactory.getLogger(BasicController.class);
 	
