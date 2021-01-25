634a5e4c17c7120dc916c9a1032a86e9e7f5f94d
susan <susan@172.29.0.148>
2021-01-25 10:49:16 -0500

    Basic auth

---
+++ com.tekntime.basic/src/main/java/com/tekntime/controller/BasicController.java
@@ -0,0 +1,65 @@
+package com.tekntime.controller;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/basic")
+public class BasicController {
+	Logger logger = LoggerFactory.getLogger(BasicController.class);
+	
+	@Value("${basic.auth.host}")
+	private String BASICAUTHHOSTNAME;
+
+	
+	@GetMapping("/app")
+    public Map<String, String> getApp(HttpServletRequest request) {
+		logger.info("reached basic-auth app" );
+		Map<String, String> map = new HashMap<String, String>();
+		if(! BASICAUTHHOSTNAME.equalsIgnoreCase(request.getHeader("host"))||
+			!"https".equalsIgnoreCase(request.getScheme())
+		  ) {
+			map.put("app", "BASIC-AUTH is NOT accessible");
+			return map;
+	    }
+		
+		map.put("scheme", request.getScheme());
+		map.put("server", request.getServerName());
+		map.put("method", request.getMethod());
+		map.put("contextpath", request.getContextPath());
+		map.put("protocol", request.getProtocol());
+		map.put("port", ""+request.getServerPort());
+		map.put("LocalName", request.getLocalName());
+		map.put("requestURI", request.getRequestURI());
+		map.put("queryString", request.getQueryString());
+		map.put("cookies", Arrays.toString(request.getCookies()));
+		
+		Enumeration<String> params = request.getParameterNames(); 
+		while(params.hasMoreElements()){
+		 String paramName = params.nextElement();
+		 map.put("Parameter Name - "+paramName, "Value - "+request.getParameter(paramName));
+		}
+		
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        map.put("app", "BASIC-AUTH");
+       
+        return map;
+    }
+
+}
--- com.tekntime.basic/src/main/java/com/tekntime/controller/LoginController.java
+++ com.tekntime.basic/src/main/java/com/tekntime/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.tekntime.controller;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,10 +28,42 @@ public class LoginController {
 	private UserLoginService service;
 	
 	@GetMapping("/app")
-    public String getApp() {
-		logger.info("reached basic-auth login app" );
-        return "basicauth";
+    public Map<String, String> getApp(HttpServletRequest request) {
+		logger.info("reached basic-auth app" );
+		Map<String, String> map = new HashMap<String, String>();
+		if(! BASICAUTHHOSTNAME.equalsIgnoreCase(request.getHeader("host"))) {
+			map.put("app", "BASIC-AUTH is NOT accessible");
+			return map;
+	    }
+		
+		map.put("scheme", request.getScheme());
+		map.put("server", request.getServerName());
+		map.put("method", request.getMethod());
+		map.put("contextpath", request.getContextPath());
+		map.put("protocol", request.getProtocol());
+		map.put("port", ""+request.getServerPort());
+		map.put("LocalName", request.getLocalName());
+		map.put("requestURI", request.getRequestURI());
+		map.put("queryString", request.getQueryString());
+		map.put("cookies", Arrays.toString(request.getCookies()));
+		
+		Enumeration<String> params = request.getParameterNames(); 
+		while(params.hasMoreElements()){
+		 String paramName = params.nextElement();
+		 map.put("Parameter Name - "+paramName, "Value - "+request.getParameter(paramName));
+		}
+		
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        map.put("app", "BASIC-AUTH");
+       
+        return map;
     }
+
 	
 	@RequestMapping( path="/read", method=RequestMethod.GET)
 	public UserLogin read( @RequestParam String loginName) {
