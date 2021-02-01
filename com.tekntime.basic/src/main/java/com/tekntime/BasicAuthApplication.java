60e236b956956bd903d1e52ace5f63f7224560a1
susan <susan@172.29.0.148>
2021-01-31 21:51:53 -0500

    Working - model without spring security

--- com.tekntime.basic/src/main/java/com/tekntime/BasicAuthApplication.java
+++ com.tekntime.basic/src/main/java/com/tekntime/BasicAuthApplication.java
@@ -3,9 +3,6 @@ package com.tekntime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
--- com.tekntime.basic/src/main/java/com/tekntime/config/WebSecurityConfig.java
+++ com.tekntime.basic/src/main/java/com/tekntime/config/WebSecurityConfig.java
@@ -1,31 +1,31 @@
 package com.tekntime.config;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@PropertySource(value = "classpath:application.properties")
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/basic/swagger-ui.html").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login").permitAll()
-				.and()
-			.logout()
-				.permitAll();
-	}
-
-}
+//@Configuration
+//@PropertySource(value = "classpath:application.properties")
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//			.authorizeRequests()
+//				.antMatchers("**/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll() // whitelist Swagger UI resources
+//				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/login").permitAll()
+//				.and()
+//			.logout()
+//				.permitAll();
+//	}
+//
+//}
