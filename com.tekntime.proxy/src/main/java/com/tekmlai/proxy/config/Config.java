904bbd7e253954bbb9190b031ef366a0d3049124
susan <susan@DESKTOP-NC0UMDI>
2021-02-02 22:29:17 -0500

    Filters added

---
+++ com.tekntime.proxy/src/main/java/com/tekmlai/proxy/config/Config.java
@@ -0,0 +1,13 @@
+package com.tekmlai.proxy.config;
+
+public  class Config {
+	private String name;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
