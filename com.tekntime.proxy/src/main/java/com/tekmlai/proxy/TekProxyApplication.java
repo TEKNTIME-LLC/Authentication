ce1ff72e66f515efb33d9a53451158ff9b6d0433
susan <susan@DESKTOP-NC0UMDI>
2021-02-02 22:28:29 -0500

    Proxy service

--- com.tekntime.proxy/src/main/java/com/tekmlai/proxy/TekProxyApplication.java
+++ com.tekntime.proxy/src/main/java/com/tekmlai/proxy/TekProxyApplication.java
@@ -3,16 +3,15 @@ package com.tekmlai.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @RestController
-public class Application extends SpringBootServletInitializer{
+public class TekProxyApplication { //can not be deployed as WAR
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(TekProxyApplication.class, args);
 	}
 
 }
